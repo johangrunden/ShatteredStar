aa92a36f2244d33ff1f26e572b197c6802a2fe41
johangrunden <grunden.johan@gmail.com>
2025-08-05 11:16:19 +0200

    World and Boss

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -4,7 +4,7 @@ var target := null
 @export var speed := 100.0
 
 func _physics_process(delta):
-    if target and is_instance_valid(target):
-        var dir = (target.global_position - global_position).normalized()
-        velocity = dir * speed
-        move_and_slide()
+	if target and is_instance_valid(target):
+		var dir = (target.global_position - global_position).normalized()
+		velocity = dir * speed
+		move_and_slide()
--- network/MultiplayerSpawner.gd
+++ network/MultiplayerSpawner.gd
@@ -3,15 +3,15 @@ extends Node2D
 @export var player_scene: PackedScene
 
 func _ready():
-    if multiplayer.is_server():
-        multiplayer.peer_connected.connect(_on_peer_connected)
-        spawn_player(multiplayer.get_unique_id())
+	if multiplayer.is_server():
+		multiplayer.peer_connected.connect(_on_peer_connected)
+		spawn_player(multiplayer.get_unique_id())
 
 func _on_peer_connected(id):
-    spawn_player(id)
+	spawn_player(id)
 
 func spawn_player(id):
-    var player = player_scene.instantiate()
-    add_child(player)
-    player.set_multiplayer_authority(id)
-    player.global_position = Vector2(100 + id * 50, 100)
+	var player = player_scene.instantiate()
+	add_child(player)
+	player.set_multiplayer_authority(id)
+	player.global_position = Vector2(100 + id * 50, 100)
